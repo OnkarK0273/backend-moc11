c6b57d899498c2c4629b88314b42d6516391ee68
onkar-kalsannawar <onkarok0273@gmail.com>
2023-05-09 15:32:02 +0530

    adding backend

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const userRoute = require("./routes/user.route");
+const cors = require("cors");
+const signRoute = require("./routes/sign.route");
 const loginRoute = require("./routes/login.route");
-const bookRoute = require("./routes/book.route");
-
+const employeesRoute = require("./routes/employees.route");
 
 const app = express();
 
+// global middlewers 
 app.use(express.json())
-
-app.use("/register", userRoute);
-
-app.use("/login", loginRoute);
+app.use(cors());
 
 
+// routes
 
-app.use("/books",bookRoute)
+app.use("/signup",signRoute)
+
+app.use("/login",loginRoute)
+
+
+app.use("/employees",employeesRoute)
+
 
 
 
 app.listen(4500, async () => {
     try {
-      await mongoose.connect(`mongodb+srv://onkar:onkaratlas@cluster0.xxociih.mongodb.net/moc11?retryWrites=true&w=majority`);
+      await mongoose.connect("mongodb+srv://onkar:onkaratlas@cluster0.xxociih.mongodb.net/moc12?retryWrites=true&w=majority");
       console.log("connected db");
     } catch (err) {
       console.log("not-connected");
--- routes/login.route.js
+++ routes/login.route.js
@@ -1,42 +1,41 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const UserModal = require("../modal/user.modal");
-
+const UserModal = require("../model/user.model");
 
 const loginRoute = express.Router()
 
-loginRoute.post("/",async(req,res)=>{
 
-    const {email,password} = req.body
+loginRoute.post('/',async(req,res)=>{
+    const {Email,Password} = req.body
+
 
     try{
-
-        const isLogin = await UserModal.findOne({email})
+        const isLogin = await UserModal.findOne({Email})
 
         if(!isLogin){
-            return res.status(400).send({error:"user not Registerd"})
+            return res.status(400).send({massage:"user not Registerd"})
         }else{
 
-            bcrypt.compare(password,isLogin.password,(err,result)=>{
+            bcrypt.compare(Password,isLogin.Password,(err,result)=>{
                 if(result){
-                    let token = jwt.sign({userid:isLogin._id,isAdmin:isLogin.isAdmin},'SECRET_KEY/MOC-11')
+                    let token = jwt.sign({userid:isLogin._id},'SECRET_KEY/MOC-12')
 
-                    res.status(200).send({ "token":token, "user":isLogin });
+                    res.status(200).send({ "token":token});
                 }else{
-                    res.status(400).send({error:"check your password"});
+                    res.status(400).send({massage:"check your password"});
                 }
             })
 
 
         }
 
-        
-
-
     }catch(err){
-        res.status(400).send({error:err.message})
+
+        res.status(400).json({err:err.message})
+
     }
+
 
 })
 
